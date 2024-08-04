440167d07163b0bbb2ce245818cbaeb292fc0001
Sanskar <sanskar06march@gmail.com>
2024-08-04 20:16:23 +0530

    admin side navbar

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,9 @@
+import AdminNav from "@/components/ui/custom-navbar Admin";
+
+export default async function Admin (){
+    return(
+        <div>
+            <AdminNav/>
+        </div>
+    );
+}
