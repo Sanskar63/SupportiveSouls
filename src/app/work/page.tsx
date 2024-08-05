516759a8fdc62f62a5cfad5a86026b99682aedf3
Sanskar <sanskar06march@gmail.com>
2024-08-06 00:09:17 +0530

    work loading

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -19,17 +19,19 @@ interface ContentItem {
 
 function Work() {
     const [content, setContent] = useState<ContentItem[]>([]);
+    const [isAvail, setAvail] = useState<boolean>(false);
 
     useEffect(() => {
         const fetchData = async () => {
             try {
                 const response = await axios.get("/api/Work/get");
                 setContent(response.data);
+                if (response.data.length !== 0) setAvail(true);
             } catch (error) {
                 console.log("problem in fetching data", error);
             }
         }
-        
+
         fetchData();
     }, [])
 
@@ -39,23 +41,28 @@ function Work() {
         <div className='w-[100%] flex flex-col items-center'>
             <CustomNavbar />
             <div className='h-[15vh] lg:h-[25vh]'></div>
-            
-            {
-                content.map((item) => (
-                    <div key={item._id} className='flex flex-col md:flex-row justify-center items-center bg-purple-light w-[90%] md:w-[65%] h-[75vh] md:h-[50vh] my-[3vh] rounded-2xl'>
-                        <div className='bg-slate-600 w-[80%] md:w-[45%] h-[50%] md:h-[85%] rounded-2xl my-[5%] md:my-[0%] md:mr-[5%]'>
-                            <Carousel data={item} />
-                        </div>
 
-                        <div className='w-[95%] md:w-[45%] h-[40%] md:h-[85%] rounded-2xl flex flex-col items-center justify-center'>
-                            <span className="text-xl md:text-2xl lg:text-3xl text-black font-semibold underline underline-offset-4">
-                                {item.heading}
-                            </span>
-                            <div className='h-[5%]'></div>
-                            <p className="text-sm lg:xl text-black text-center">{item.description}</p>
+            {
+                isAvail ? (
+                    content.map((item) => (
+                        <div key={item._id} className='flex flex-col md:flex-row justify-center items-center bg-purple-light w-[90%] md:w-[65%] h-[75vh] md:h-[50vh] my-[3vh] rounded-2xl'>
+                            <div className='bg-purple-mid w-[80%] md:w-[45%] h-[50%] md:h-[85%] rounded-2xl my-[5%] md:my-[0%] md:mr-[5%]'>
+                                <Carousel data={item} />
+                            </div>
+    
+                            <div className='w-[95%] md:w-[45%] h-[40%] md:h-[85%] rounded-2xl flex flex-col items-center justify-center'>
+                                <span className="text-xl md:text-2xl lg:text-3xl text-black font-semibold underline underline-offset-4">
+                                    {item.heading}
+                                </span>
+                                <div className='h-[5%]'></div>
+                                <p className="text-sm lg:xl text-black text-center">{item.description}</p>
+                            </div>
                         </div>
-                    </div>
-                ))
+                    ))
+                ) :
+                    (
+                        <p className="text-xl md:text-2xl lg:text-3xl text-black font-semibold w-[50%] lg:w-[35%] text-center">No Work Available</p>
+                    )
             }
 
             <div className='h-[10vh]'></div>
