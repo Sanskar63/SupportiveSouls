b44c74d283027ebcffae89d47a9e154194324242
Sanskar <sanskar06march@gmail.com>
2024-08-06 14:26:42 +0530

    error resolution

--- src/app/api/Work/rm/route.ts
+++ src/app/api/Work/rm/route.ts
@@ -1,6 +1,7 @@
 import dbConnect from "@/lib/dbConnect";
 import { deleteFromCloudinary } from "@/lib/upload";
 import WorkModel from "@/model/Work";
+import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
     await dbConnect();
@@ -9,20 +10,18 @@ export async function POST(request: Request) {
         const id = url.searchParams.get('id');
 
         const result = await WorkModel.findById(id);
-        // console.log(result, result?.images.length);
         if(!result){
-            return new Response("Couldn't delete", {status: 400});
+            return new Response(`Couldn't find the item with id == ${id}`, {status: 400});
         }
         let public_id;
         for(let i=0; i< result?.images.length; i++){
             public_id = await deleteFromCloudinary(result?.images[i].public_id);
-            // console.log(result?.images[i])
         }
         const deleted = await WorkModel.deleteOne({_id:id});
 
-        return new Response("sucessfully deleted the work", {status:200});
+        return NextResponse.json({message: "sucessfully deleted the work", data:deleted}, {status:200});
     } catch (error) {
         console.log(error);
-        return new Response("Some error in removing work in try catch", {status:400});
+        return NextResponse.json({message:"Some error in removing work in try catch", error:error}, {status:400});
     }
 }
